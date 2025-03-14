93f190134f0aa653ec6d6510826a378d96cd273d
Inna-Martunyuk <imartunyuk88@gmail.com>
2025-03-14 19:05:32 +0200

    task-4

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,35 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import { nanoid } from 'nanoid';
+import style from './Form.module.css';
 
-const Form = () => {
-  return <h2>Form</h2>;
+const Form = ({ onSubmit }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+      const form = e.target;
+    const inputValue = form.elements.search.value.trim();
+  
+
+    if (inputValue) {
+      onSubmit({id: nanoid, text: inputValue});
+      form.reset(); 
+    }
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,11 +1,44 @@
 import Text from '../components/Text/Text';
+import Form from "../components/Form/Form";
+import TodoList from '../components/TodoList/TodoList';
+import { useState, useEffect } from 'react';
+
+
 
 const Todos = () => {
+ const [todos, setTodos] = useState(() => {
+   const savedTodos = window.localStorage.getItem('saved-todos');
+   return savedTodos
+     ? JSON.parse(savedTodos)
+     : [
+         { id: '1', text: 'Practice more' },
+         { id: '2', text: 'Get all tasks done on time' },
+       ];
+ });
+useEffect(() => {
+  window.localStorage.setItem('saved-todos', todos);
+}, [todos]);
+ 
+  const addNewTodo = inputValue => {
+    setTodos((prevTodos) => {
+      return[...prevTodos, inputValue]
+    })
+  };
+  const deleteTodo = (todoId) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    })
+
+  }
+
   return (
     <>
-      <Text textAlign="center">There are no any todos ...</Text>
+      <Form onSubmit={addNewTodo} />
+      <TodoList todos={todos} onDelete={deleteTodo} />
+      {todos.length === 0 && (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
     </>
   );
 };
-
 export default Todos;
